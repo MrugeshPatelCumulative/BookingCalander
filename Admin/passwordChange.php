b488196037274456ee2ad789b83e78598946bfdb
MrugeshPatelCumulative <mrugeshpatelcumulative@gmail.com>
2021-04-27 20:24:12 +0530

    Update setting.php

--- Admin/passwordChange.php
+++ Admin/passwordChange.php
@@ -3,13 +3,29 @@ include '../DataBase/dataBaseFunction.php';
 include 'header.php';
 include 'afterLogin.php';
 ?>
-			<div class="container-fluid">
-				<div class="row">
-					ChangePassword
+<div class="container-fluid">
+	<form action="../ajax/ChangePassword.php" method="POST">
+		<div class="middle-formbox maxbox-width">
+			<div class="row">
+				<div class="col-3">
+					<label>Enter Password</label>
+				</div>
+				<div class="col-5">
+					<input type="password" name="newPassword" id="newPassword" required="">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<button class="btn btn-primary">
+						Change Password
+					</button>
 				</div>
 			</div>
 		</div>
-	</div>
+	</form>
+</div>
+</div>
+</div>
 </body>
 <?php
 include 'footer.php';
