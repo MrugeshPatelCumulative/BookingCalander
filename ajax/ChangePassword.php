3f8d725919bcf550fb69c93e22b0a608f727b658
MrugeshPatelCumulative <mrugeshpatelcumulative@gmail.com>
2021-04-28 12:25:33 +0530

    Create ChangePassword.php

---
+++ ajax/ChangePassword.php
@@ -0,0 +1,9 @@
+<?php
+include '../DataBase/dataBaseFunction.php';
+$password = $_POST['newPassword'];
+if(!empty($password)){
+	db_update('admindata',array('AdminPassword'=>$password),array('AdminId'=>1));
+	unset($_SESSION['Admin']);
+}
+header('Location: ../Admin/index.php');
+?>
